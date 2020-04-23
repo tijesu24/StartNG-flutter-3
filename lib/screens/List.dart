1734d3695e1ebffae96d6e02e73cf96e3ae7ca13
tijesu24 <tijesu24@gmail.com>
2020-04-23 15:30:20 +0100

    completed task

--- lib/screens/List.dart
+++ lib/screens/List.dart
@@ -1,175 +1,88 @@
-import 'dart:io';
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
-import 'package:json_serializable/json_serializable.dart';
+
 import 'package:flutter/material.dart';
+import 'package:startngflutter_3/model/person.dart';
+import 'package:startngflutter_3/screens/PersonPage.dart';
 
-
-class SavedVisit extends StatefulWidget {
-  
-
-  
-
+class PeopleList extends StatefulWidget {
   @override
-  _SavedVisitState createState() {
-    return _SavedVisitState();
+  _PeopleListState createState() {
+    return _PeopleListState();
   }
 }
 
-class _SavedVisitState extends State<SavedVisit> {
-  
+class _PeopleListState extends State<PeopleList> {
+  Future<List<Person>> futurePeople;
+
   @override
-void initState() {
+  void initState() {
     super.initState();
-    pq = PersistentQueue('visitQueue' , onFlush: (list) async {
-    final result = await InternetAddress.lookup('google.com');
-  if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-    if (list[0] == events[0]) {
-      await _onlineDatabase.addVisit((){
-        for 
-      }
-  
-    }}
-  print('auto-flush\n$list');
-});
-}
-
-  Future getAllVisits() async{
-    visits = 
-    
-    return visits ;
+    futurePeople = fetchPeople();
   }
 
-  _screenReturn (){
-    if(visits == null) return SingleChildScrollView(physics: AlwaysScrollableScrollPhysics());
+  _screenReturn(List<Person> peopleList) {
+    if (peopleList == null)
+      return SingleChildScrollView(physics: AlwaysScrollableScrollPhysics());
     else {
-      if(visits.length == 0) return SingleChildScrollView(physics: AlwaysScrollableScrollPhysics());
-      else{
-      return ListView.builder
-  (
-    itemCount: visits != null ?visits.length : 0,
-    itemBuilder: (BuildContext ctxt, int index) {
-     return ListTile(
-            title: new Text(visits[index].siteId,
-      style: TextStyle(fontSize: 16), ),
-            subtitle: Container(
-                child: Text(visits[index].dateofVisit.toIso8601String(),
-        style: TextStyle(color: Colors.brown, fontSize: 12)),
-            ),
-             onTap: (){openVisitForm(visits[index]);},
-             trailing: Row(
-               mainAxisSize: MainAxisSize.min,
-       children: <Widget>[
-         Icon(visits[index].pending ? Icons.cached : Icons.check),
-         Icon(Icons.more),
-       ],
-             )) ;
-             
+      if (peopleList.length == 0)
+        return SingleChildScrollView(physics: AlwaysScrollableScrollPhysics());
+      else {
+        return ListView.builder(
+            itemCount: peopleList != null ? peopleList.length : 0,
+            itemBuilder: (BuildContext ctxt, int index) {
+              return ListTile(
+                  title: new Text(
+                    peopleList[index].name,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  subtitle: Container(
+                    child: Text(peopleList[index].email,
+                        style: TextStyle(color: Colors.brown, fontSize: 12)),
+                  ),
+                  onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> PersonPage(peopleList[index]) ));});
+            });
+      }
+    }
   }
-     );}}}
-
-  
-  Future<http.Response> fetchAlbum() {
-  return http.get('https://jsonplaceholder.typicode.com/users');
-}
 
   @override
-  Widget build (BuildContext context) {
-    
+  Widget build(BuildContext context) {
     //openVisitForm();
-    return FutureBuilder(
-          future: initialiseDB().whenComplete((){getAllVisits();}),
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-           if(snapshot.hasData) {
-            return new 
-             Scaffold(
-        key: scaffoldKey,
-        appBar: AppBar(
-          title: const Text('Site Inspection'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.add_alert),
-              tooltip: 'Add Visit',
-              onPressed: () {
-                //openVisitForm();
-                updateOnlineDatabase();
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.more_horiz),
-              tooltip: 'Next page',
-              onPressed: () async{
-                await widget._auth.signOut();
-                 /* dropDB();
-                setState(() {
-                  getAllVisits(); 
-                }); */                
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.airline_seat_recline_extra),
-              tooltip: 'Add',
-              onPressed: ()=> testMultiple(),
-            ),
-            
-          ],
-        ),
-        body: _screenReturn(),
+    return Scaffold(
+        appBar: AppBar(title: Text('People')),
+        body: FutureBuilder(
+            future: futurePeople,
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if (snapshot.hasData) {
+                return _screenReturn(snapshot.data);
+              } else if (snapshot.hasError)
+                return new Text('Error: ${snapshot.error}');
+              else
+                return new Text('Loading....');
+            }));
+  }
 
-);}
-    else if (snapshot.hasError)
-                  return new Text('Error: ${snapshot.error}');
-    else
-          return new Text('Loading....');
-            
-              });}
-    
-           
-    
-  
-  openVisitForm([SiteVisit visitb4])async{
-                      SiteVisit visit = //SiteVisit.fromList(['14c926t0-39dd-11ea-l39i-fff47b71598b', 'oy066', '2020-01-18T11:28:30.825026', 32323.0, 23.0, 323.0, 1, 232.0, 323.0, 34.0, 1, 1, 323.0, 213.0, 23.0, 1, 0, 2131.0, 12.0, 1, 1, 1, '',1]);
-                      await Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => VisitForm(visitb4),
-              ));
-              if(visit != null){
-              await visitDB.insertSiteVisit(visit);
-              setState(() {
-                getAllVisits();
-              }); 
-              } 
-          }
+  Future<List<Person>> fetchPeople() async {
+    final response =
+        await http.get('https://jsonplaceholder.typicode.com/users');
 
-   testMultiple()async{
-                      List<SiteVisit> visits = [SiteVisit.fromList(['14c926t0-39dd-11ea-l39i-fff47b71598b', 'oy066', '2020-01-18T11:28:30.825026', 32323.0, 23.0, 323.0, 1, 232.0, 323.0, 34.0, 1, 1, 323.0, 213.0, 23.0, 1, 0, 2131.0, 12.0, 1, 1, 1, '',1]),
-                      SiteVisit.fromList(['14c926t0-39dd-11ea-l66i-fff47b71598b', 'oy066', '2020-01-18T11:28:30.825026', 32323.0, 23.0, 323.0, 1, 232.0, 323.0, 34.0, 1, 1, 323.0, 213.0, 23.0, 1, 0, 2131.0, 12.0, 1, 1, 1, '',1]),
-                      SiteVisit.fromList(['14c926t0-39dd-11ea-l3fi-fff47b71598b', 'oy109', '2020-01-18T11:28:30.825026', 32323.0, 23.0, 323.0, 1, 232.0, 323.0, 34.0, 1, 1, 323.0, 213.0, 23.0, 1, 0, 2131.0, 12.0, 1, 1, 1, '',1]),
-                      SiteVisit.fromList(['14c926t0-39dd-11ea-l39i-fffijb71598b', 'og666', '2020-01-18T11:28:30.825026', 32323.0, 23.0, 323.0, 1, 232.0, 323.0, 34.0, 1, 1, 323.0, 213.0, 23.0, 1, 0, 2131.0, 12.0, 1, 1, 1, '',1]),
-                      SiteVisit.fromList(['14c92y00-39dd-11ea-l39i-fff47b71598b', 'oy010', '2020-01-18T11:28:30.825026', 32323.0, 23.0, 323.0, 1, 232.0, 323.0, 34.0, 1, 1, 323.0, 213.0, 23.0, 1, 0, 2131.0, 12.0, 1, 1, 1, '',1])
-                      ];
-
-                      visits.forEach((it) async =>await visitDB.insertSiteVisit(it));
-            
-              setState(() {
-                getAllVisits();
-              }); 
-              } 
-          
-    
-    dropDB (){
-      visitDB.database.execute("DROP TABLE IF EXISTS visits");
-      visitDB.database.execute(
-          "CREATE TABLE visits(entryuid TEXT PRIMARY KEY, siteId TEXT, dateofVisit TEXT, dgCapacity REAL, genRH REAL, lastPPM REAL, dgDuePPM INTEGER, freq REAL, dgACVolts REAL, dgACLoad REAL, engOilOk INTEGER, radOk INTEGER, dieselDipLts REAL, dieselDipcm REAL, dieselGal REAL, phcnOk INTEGER, hybridOk INTEGER, dcLoad REAL, moduleNo REAL, secLtOk INTEGER, avLtOk INTEGER, janitOk INTEGER, comment TEXT, pending INTEGER)");
-    }
-
-    updateOnlineDatabase(){
-       setState( (){for (SiteVisit item in visits) {
-        if(item.pending){
-          widget._onlineDatabase.addVisit(item);
-          item.pending = false;
-          print(item.siteId);
-        }
-      }});
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return compute(parsePeople, response.body);
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load Person');
     }
   }
+}
+  List<Person> parsePeople(String responseBody) {
+    final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+    return parsed.map<Person>((json) => Person.fromJson(json)).toList();
+  }
+
--- lib/screens/PersonPage.dart
+++ lib/screens/PersonPage.dart
@@ -1,30 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:startngflutter_3/model/person.dart';
 
 
-class PersonForm extends StatefulWidget {
-  
-  
-  
+
+
+class PersonPage extends StatelessWidget {
+final Person person ;
+PersonPage(this.person);
 
   @override
-  PersonFormState createState() => PersonFormState();
-}
-
-class PersonFormState extends State<PersonForm> {
-  
-  @override
-          void initState() {
-                super.initState();
-                
-          }
-
-  
-
-    
-
-    @override
   Widget build(BuildContext context) {
-      return Text('We will rock you')
+    return Scaffold(appBar: AppBar(title:Text(person.name)),
+    body: Column(
+      children: <Widget>[
+        CircleAvatar(backgroundImage: AssetImage('assets/images/person_avatar.jpg'),radius: 100,),
+        SizedBox(height: 20,),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Name: ' + person.name),          
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.mail),
+            title: Text('Email: ' + person.email),          
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('Phone number: ' + person.phoneno),          
+          ),
+        ),
+
+        
+        
+        
+      ],
+    ),);
   }
-  
 }
