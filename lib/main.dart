63c65d62c6cc8bd865e2bcf0d4ea3f48a73531bc
tijesu24 <tijesu24@gmail.com>
2020-04-24 09:23:10 +0100

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Color.fromARGB(255, 202,254,254),
-        // Color.fromARGB(a, )
+        
+        primarySwatch: Colors.blueGrey[50],
+        cardColor: Colors.green
       ),
       home: PeopleList()
     );
